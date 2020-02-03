664271372eab39adeb2626439dccd4a36817eff6
happy322 <technik.rek@gmail.com>
2020-02-04 00:10:46 +0300

    taskGetandSet update v1.0

--- taskGetandSet.py
+++ taskGetandSet.py
@@ -3,4 +3,7 @@ import json
 
 class Task:
     def __init__(self, task_value):
-        pass
+        self.keywords = None
+        self.category = None
+        self.color = None
+        self.size = None
